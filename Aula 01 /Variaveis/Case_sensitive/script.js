40f42f377bb118f100d1310748c321217b62b277
Pedro Sebben <dev.pedro_sebben@outlook.com.br>
2025-08-15 19:46:00 -0300

    Aula 01 - Variaveis

---
+++ Aula 01 /Variaveis/Case_sensitive/script.js
@@ -0,0 +1,12 @@
+// Quando uma linguagem de programacao é 
+// case sensitive significa que ela é sensivel a letra maíuscula
+
+// por exemplo: "Pedro" é considerado diferente de "pedro"
+
+
+var product = "Teclado Mecanico"
+
+console.log(price);
+
+
+
---
+++ Aula 01 /Variaveis/script.js
@@ -0,0 +1,21 @@
+// Declarar uma váriavel no JS sem valor
+
+var user 
+
+console.log(user);
+
+
+// Declarando uma Variável com valor
+
+var email = "pedro.sebben@universo.univates.br"
+
+console.log(email);
+
+
+// substitui o valor de uma Variável
+
+email = "teste@gmail.com"
+
+console.log(email);
+
+
