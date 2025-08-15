3a4e8c5d1cd2d1b09ae0d457e96009a4795f807d
Pedro Sebben <dev.pedro_sebben@outlook.com.br>
2025-08-15 20:27:12 -0300

    Escopo

---
+++ Aula 01 /Variaveis/Escopo/script.js
@@ -0,0 +1,11 @@
+// Escopo é a região do código onde uma determinada váriavel é acessível ou visível
+//  O escopo define o contexto no qual uma variável pode ser referenciada ou modificada 
+// Tente pensar no contexto como cômodo de uma casa. Por exemplo, para utilizar o fogao voce deve estar
+// na cozinha . 
+
+
+// Escopo Global: variáveis declaradas fora de qualquer funcao ou bloco de codigo (VAR)
+// Escopo Bloco : Acessíveis apenas dentro do bloco de código onde foram  declaradas (LET E CONST)
+// Escopo Local: (ou escopo de função) variáveis declaradas dentro de uma função 
+
+
