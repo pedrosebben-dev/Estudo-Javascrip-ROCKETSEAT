3290b8027356702c74e9db7391407442b4194f6f
Pedro Sebben <dev.pedro_sebben@outlook.com.br>
2025-08-15 20:03:16 -0300

    Let and Const

---
+++ Aula 01 /Variaveis/Const/script.js
@@ -0,0 +1,13 @@
+// Constante cria um valor de Váriavel Fixo!! Náo pode ser alterado 
+
+
+const number = 42
+
+console.log(number);
+
+
+// se eu rodar o projeto verei que no log haverá um alerta de erro, porque diferente do que foi visto
+//  antes, o Const náo pode ser alterado de valor 
+number = 55
+
+console.log(number);
---
+++ Aula 01 /Variaveis/Let/script.js
@@ -0,0 +1,20 @@
+// Declara uma váriavel sem valor
+
+let user 
+
+console.log("Pedro")
+
+
+// Declarar uma váriavel com valor
+// let diferente do VAR  nao permite que seja subscrito variáveis com o mesmo nome !!
+let email = "pedro.sebben@gmail.com"
+
+console.log(email);
+
+email = "joao@gmail.com"
+
+console.log(email);
+
+// como pode perceber let está com outro valor atribuido e nao sobrescrevendo o mesmo de Pedro !!
+
+
