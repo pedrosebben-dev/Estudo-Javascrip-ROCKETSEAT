efd3c5ca08adddbaaf8c2b28186aab742eede161
Pedro Sebben <dev.pedro_sebben@outlook.com.br>
2025-08-16 09:12:47 -0300

    Undefined e Null

---
+++ Aula 01 /Variaveis/Undefined_Null/script.js
@@ -0,0 +1,13 @@
+// Undefined (indefinido) e Null
+
+let emptiness
+
+console.log("o valor é", emptiness)
+
+// quando uma variavel esta sem valor (ou indefinida) é Undefined
+
+let empty = null
+
+console.log("o valor é ", empty)
+
+// null nos declaramos quando queremos que ela seja vazia 
