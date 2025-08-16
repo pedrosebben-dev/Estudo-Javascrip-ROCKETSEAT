2fa954cc41fdc8fd6c8c55b40e1e841b5a8754fc
Pedro Sebben <dev.pedro_sebben@outlook.com.br>
2025-08-15 22:20:10 -0300

    Tipo de Dados

---
+++ Aula 01 /Variaveis/Tipo_de_dados/script.js
@@ -0,0 +1,10 @@
+// Tipos de Dados: Javascript é uma tipagem dinâmica com tipos 
+// dinâmicos. Isso significa que o tipo da variável é definida
+// dinamicamente peli tipo do valor atribuído (não é necessário declarar o tipo)
+
+// além disso, é possível reatribuir uma mesma variável com um tipo diferente 
+
+
+// Tipos primitivos do Javascript - veremos um a um na sequencia .....
+
+// string - number - boolean - null - undefined
