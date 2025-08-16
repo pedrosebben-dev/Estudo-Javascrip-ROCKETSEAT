55844c4b1d619b09d2c276ee55b2de937abe93c0
Pedro Sebben <dev.pedro_sebben@outlook.com.br>
2025-08-15 22:06:04 -0300

    Nome de variável

---
+++ Aula 01 /Variaveis/Nome_de_Variaveis/script.js
@@ -0,0 +1,47 @@
+// case - sensitive !!
+
+let username = "Pedro"
+let userName = "joao"
+
+console.log(username);
+console.log(userName);
+
+// podemos 
+
+let $email = "teste@gmail.com"
+let _email = "teste2@gmail.com"
+let user_email = "teste3@gmail.com"
+
+// podemos, mas nao é recomendado por boa pratica do projeto (usar ascento pode dar problema )
+let Ação = "Cadastrar" 
+
+console.log(Ação)
+console.log(user_email);
+console.log($email);
+console.log(_email);
+
+// NAO podemos 
+
+// let 1user = "jose" - criar variavel comecando por numero 
+// let product Name = "juice" - criar variável com espaco entre o nome
+
+
+// Recomendacoes 
+//  pra nome de variavel sempre escrever em Ingles, é padrao universal 
+// e para as empresas é quase regra no mercado de trabalho !!! 
+// Sempre dar nome para variável que faça sentido referente ao que esta fazendo no projeto !!! 
+// Se irá criar uma variável para colocar nome em produto, use ProductName por exemplo 
+// O que remete a outra recomendacao e padrao de empresas, camelCase, sempre primeira letra minuscula
+// e tudo junto, quando comecar a outra colocar em maiúscula!!! 
+
+let student = "Pedro"
+
+console.log(student);
+
+let productName = "Coke"
+
+console.log(productName);
+
+let lastName = "Sebben"
+
+console.log(lastName);
