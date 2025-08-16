12e864cfc97f9c64e3a023e7a332155288ffa1b4
Pedro Sebben <dev.pedro_sebben@outlook.com.br>
2025-08-15 22:26:39 -0300

    String

---
+++ Aula 01 /Variaveis/String/script.js
@@ -0,0 +1,18 @@
+// string
+
+let username = "Pedro"
+
+console.log(username);
+console.log(typeof username);
+
+console.log("uma string com aspas");
+console.log('uma string com apóstrofo (aspas simples)');
+
+console.log('uma aspas com "aspas duplas" dentro de uma simples');
+
+console.log("uma string com 'aspas simples' dentro de uma dupla");
+
+console.log(`
+  uma string com acento grave permite
+  escrever em multiplas linhas   
+    `)
