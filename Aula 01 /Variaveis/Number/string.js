4ce511b4a3c9e29a856f7c8e482ca7841594c7f6
Pedro Sebben <dev.pedro_sebben@outlook.com.br>
2025-08-16 09:05:04 -0300

    Number

---
+++ Aula 01 /Variaveis/Number/string.js
@@ -0,0 +1,10 @@
+// Number
+
+console.log(typeof 5)
+
+// Voce pode guardar valores inteiros positivo
+// voce pode armazenar inteiro negativo
+// voce pode fazer com numeros reais (float)
+
+console.log(-5);
+console.log(125.80);
