e52c423ce82b4a268e30599c89c308427f52e74f
Pedro Sebben <dev.pedro_sebben@outlook.com.br>
2025-08-17 16:07:11 -0300

    Operadores aritímeticos

---
+++ Aula 02/Expressões_e_Operadores/Operdadores_aritimeticos/script.js
@@ -0,0 +1,25 @@
+// Operadores Aritiméticos
+
+console.log("Soma: ", 12 + 5)
+// soma é dirfente de concatenacao !!!
+
+console.log("Concatenacao: ", "12" + "5") // aqui vai mostrar 125 
+
+
+// subtracao 
+console.log("Subtracao: ", 12 - 5)
+
+// Multiplicação
+console.log("Multiplicação: ", 12 * 5)
+
+
+// Divisão
+console.log("Divisão: ", 12 / 5)
+
+
+// Resto da Divisão
+console.log("Resto da Divisão: ", 12 - 5)
+
+
+// Exponencial
+console.log("Exponencial: ", 12 ** 2)
