d98fdd8458e19bf36a37e56109a454f0a026a930
Pedro Sebben <dev.pedro_sebben@outlook.com.br>
2025-08-17 16:18:11 -0300

    Incremento  e decremento

---
+++ Aula 02/Expressões_e_Operadores/Incremento_decremento/script.js
@@ -0,0 +1,27 @@
+let number = 10
+
+
+// INCREMENTO 
+
+// number = number + 1
+
+number ++
+
+
+// Incrementa após por isso nao mostra no console
+console.log(number++)
+console.log(number)
+
+
+// Incrementa antes
+
+console.log(++number)
+
+
+// Decremento
+
+console.log("Decremento após ", number--)
+console.log(number--)
+
+// Decremento Antes
+console.log(--number)
